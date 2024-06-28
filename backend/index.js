dee69ade91f07eda391ce9f484863ddb21077c46
Harsh Mehta <111949832+fenrir2608@users.noreply.github.com>
2024-06-29 03:06:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ mongoose
 
 app.use(
     cors({
-        origin: 'https://seedstake.onrender.com',
+        origin: 'https://seedstake.vercel.app',
         credentials: true,
         methods: ['GET','POST','PUT','DELETE'],
         allowedHeaders: ['Content-Type']
