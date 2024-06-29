dacd4a89cc924839aeaab2eb3dfc12bc161dc910
Harsh Mehta <111949832+fenrir2608@users.noreply.github.com>
2024-06-29 16:15:41 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ mongoose
 
 app.use(
     cors({
-        origin: 'http://localhost:5173/',
+        origin: 'http://localhost:5173',
         credentials: true,
         methods: ['GET','POST','PUT','DELETE'],
         allowedHeaders: ['Content-Type']
